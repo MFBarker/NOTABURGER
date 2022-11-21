7f22cfa6174aa5f67d5a118b6be73626123305c9
MFBarker <98473836+MFBarker@users.noreply.github.com>
2022-11-21 11:52:03 -0700

    Menu Secret Code Implemented + Folder Added for Menu Pictures

--- Scripts/menu_JS.js
+++ Scripts/menu_JS.js
@@ -17,45 +17,22 @@ for (i = 0; i < coll.length; i++) {
     });
 }
 
+//button for secret button
+function addClick() {
+    var addMenu = document.getElementById("AddButton");
+    var B = document.getElementById("B");
+    var K = document.getElementById("K");
+    var F = document.getElementById("F");
+    var L = document.getElementById("L");
 
-// item int display selection
-var count = 1;
+    var secret = document.getElementById("secret");
 
-var minus = document.getElementById("minus");
-var plus = document.getElementById("plus");
-var num_display = document.getElementById("numDisplay").ariaValueText;
-
-
-if (num_display == 1) { minus.disabled = true; }
-else if (num_display == 8) { plus.disabled = true; }
-else if (num_display > 1 && num_display < 8) { minus.disabled = false; plus.disabled = false; }
-
-
-
-function up() {
-    if (num_display == 1) { count = 2; }
-    else if (num_display == 2) { count = 3; }
-    else if (num_display == 3) { count = 4; }
-    else if (num_display == 4) { count = 5; }
-    else if (num_display == 5) { count = 6; }
-    else if (num_display == 6) { count = 7; }
-    else if (num_display == 7) { count = 8; plus.ariaDisabled = true; }
-
-    //debug only
-    console.log(num_display + " (up)");
-    num_display.ariaValueText = count;
+    addMenu.addEventListener("click", function () {
+        this.classList.toggle("active");
+        if (B.checked == 1 && K.checked == 1 && F.checked == 1 && L.checked == 1) {
+            console.log("number 15");
+            secret.textContent = "Code Goes Here";
+        }
+        else { console.log("click"); }
+    });
 }
-
-function down() {
-    if (num_display == 8) { count = 7; }
-    else if (num_display == 7) { count = 6; }
-    else if (num_display == 6) { count = 5; }
-    else if (num_display == 5) { count = 4; }
-    else if (num_display == 4) { count = 3; }
-    else if (num_display == 3) { count = 2; }
-    else if (num_display == 2) { count = 1; minus.ariaDisabled = true; }
-
-    //debug only
-    console.log(num_display + " (down)")
-}
-
